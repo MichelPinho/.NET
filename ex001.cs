332fabe0a6737ca68388fb6064a6c1698f85f2ea
Michel Pinho <jologpinho@gmail.com>
2021-02-23 16:57:41 -0300

    Create ex001.cs

---
+++ ex001.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace instrucoes
+{
+   class Program 
+   {
+       static void Main (string[] args)
+       {
+           int a;
+           int b = 2, c = 3;
+           const int d = 4;
+           a = 1;
+           Console.WriteLine(a+b+c+d);
+
+       }
+   }
+}
